2cddeb4fc4d2b13fb03b6b0d1358d03b53a18f88
kascez <zorankascelan60@gmail.com>
2019-11-19 23:49:45 +0100

    slider

--- dist/particles.c7cea895.js
+++ dist/particles.c7cea895.js
@@ -1754,7 +1754,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51491" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49705" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
